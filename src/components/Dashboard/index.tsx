6333c0d5f3cd2049f8ab06a109f9f42a846ba2df
thiago <thiagofreitascarneiro@hotmail.com>
2021-11-14 17:47:09 -0300

    creating transactions to deposit and withdraw

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,12 +1,12 @@
 import { Summary } from "../Summary"
-import { TransactionTabble } from "../TransactionsTable";
+import { TransactionsTable } from "../TransactionsTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
-            <TransactionTabble />
+            <TransactionsTable />
         </Container>
 
     );
